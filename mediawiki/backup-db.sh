e484a57e27409863b9dc1119c1b12b00a56f50ad
zital <zital@riseup.net>
2024-10-14 15:32:08 +0200

    remove dupplicated backup

--- mediawiki/backup-db.sh
+++ mediawiki/backup-db.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 source env.sh
 CONTAINER="${NAME}-mariadb-${MEDIAWIKI_VF}"
-DB=$NAME
 DATE=`date +%Y-%m-%d--%H-%M-%S`
-mkdir -p backup/db
-${COMMAND} exec -it ${CONTAINER} mariadb-dump --databases ${DB} --add-drop-database --routines --triggers --user=root > backup/db/${DB}-${DATE}.sql
+BACKUP_DIR="backup/db"
+mkdir -p $BACKUP_DIR
+
+LAST_BACKUP="${BACKUP_DIR}/`ls -r1 ${BACKUP_DIR} | grep .sql$ | head -n 1`"
+LAST_HASH=`md5sum $LAST_BACKUP | awk '{print $1}'`
+
+TMP_BACKUP="${BACKUP_DIR}/tmp.sql"
+TARGET_BACKUP="${BACKUP_DIR}/${DB}-${DATE}.sql"
+
+${COMMAND} exec -it ${CONTAINER} mariadb-dump --databases ${NAME} --add-drop-database --routines --triggers --user=root > ${TMP_BACKUP}
+CURRENT_HASH=`md5sum ${TMP_BACKUP} | awk '{print $1}'`
+
+echo "Backup Database:"
+echo ""
+echo "Last Backup file: ${LAST_BACKUP}"
+echo "Last Hash:        ${LAST_HASH}"
+echo "Current Hash:     ${LAST_HASH}"
+echo ""
+
+if [ "$LAST_HASH" != "$CURRENT_HASH" ]; then
+    mv "${TMP_BACKUP}" "${TARGET_BACKUP}"
+    echo "Backup saved at:  ${TARGET_BACKUP}"
+else
+    echo "Same as last backup, removing it"
+    rm $TMP_BACKUP
+fi
+
+echo "Done."
+
+exit 0
