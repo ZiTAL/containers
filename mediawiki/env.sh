f2260cd112dce92ee9310df7310c34142acf64a5
zital <zital@riseup.net>
2024-11-05 15:28:40 +0100

    env + readme

--- mediawiki/env.sh
+++ mediawiki/env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 NAME="mediawiki"
-MEDIAWIKI_VS="1.42"
-MEDIAWIKI_VF="${MEDIAWIKI_VS}.3"
+MEDIAWIKI_VF="1.42.3"
+MEDIAWIKI_VS="${MEDIAWIKI_VF%.[0-9]*}"
 MEDIAWIKI_EXT="REL${MEDIAWIKI_VS//./_}"
 COMMAND="podman"
 COMMAND_COMPOSE="podman-compose"
 CONFIG_FILE="docker-compose.yml"
-#CONFIG_FILE="docker-compose-2.yml"
 CONTAINERS=("data" "memcached" "mariadb" "php" "nginx")
 PORT=8081
 
+echo $MEDIAWIKI_VS
